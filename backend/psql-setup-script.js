928ab517de22ebe92277c3fe433df615872bb461
Nicolas Borromeo <nicoborrom@gmail.com>
2024-07-08 12:31:35 -0700

    console

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -1,5 +1,9 @@
 const { sequelize } = require('./db/models');
 
+console.log('Current working directory:', process.cwd());
+console.log('Trying to load sequelize:', require.resolve('sequelize'));
+
+
 sequelize.showAllSchemas({ logging: false }).then(async (data) => {
     if(!data.includes(process.env.SCHEMA)) {
         await sequelize.createSchema(process.env.SCHEMA);
