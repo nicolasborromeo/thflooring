eb47d71eb870165ab530d6c5dea46c7df0868bb8
Nicolas Borromeo <nicoborrom@gmail.com>
2024-07-08 14:54:04 -0700

    f

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,17 +10,13 @@ export default defineConfig(({ mode }) => ({
       failOnError: mode === "production"
     })
   ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:8000',
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, '')
+  //     }
+  //   }
+  // },
 }));
