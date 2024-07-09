02e7c5393a709f913a0b3f586c01eb4de5467f02
Nicolas Borromeo <nicoborrom@gmail.com>
2024-07-09 08:34:16 -0700

    csrg

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === 'production') {
     const path = require('path');
     // Serve the frontend's index.html file at the root route
     router.get('/', (req, res) => {
-      res.cookie('XSRF-TOKEN', req.csrfToken());
+      // res.cookie('XSRF-TOKEN', req.csrfToken());
       res.sendFile(
         path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
       );
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === 'production') {
 
     // Serve the frontend's index.html file at all other routes NOT starting with /api
     router.get(/^(?!\/?api).*/, (req, res) => {
-      res.cookie('XSRF-TOKEN', req.csrfToken());
+      // res.cookie('XSRF-TOKEN', req.csrfToken());
       res.sendFile(
         path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
       );
