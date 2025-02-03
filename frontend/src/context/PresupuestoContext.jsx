d90baf26af0174b8cddd4e1b0f3957286aa96c6d
Nicolas Borromeo <nicoborrom@gmail.com>
2025-02-03 19:26:03 -0300

    context init

---
+++ frontend/src/context/PresupuestoContext.jsx
@@ -0,0 +1,15 @@
+import { createContext, useState } from "react";
+
+
+const PresupuestoContext = createContext()
+
+function PresupuestoProvider() {
+    const [total, setTotal] = useState()
+    const [prodDetails, setProdDetails] = useState()
+    const [print, setPrint] = useState()
+
+    const value = {
+
+    }
+
+}
