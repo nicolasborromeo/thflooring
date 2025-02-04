02961cff9c1828f9988b9105266b8b6e6ea8dc80
Nicolas Borromeo <nicoborrom@gmail.com>
2025-02-04 13:18:48 -0300

    initializing with empty array

--- frontend/src/context/ProductsContext.jsx
+++ frontend/src/context/ProductsContext.jsx
@@ -4,17 +4,17 @@ import { createContext, useContext, useEffect, useState } from "react";
 const ProductContext = createContext()
 
 export function ProductsProvider ({children}) {
-    const [products, setProducts] = useState()
+    const [products, setProducts] = useState([])
 
     useEffect(()=> {
         const fetchProducts = async () => {
             try {
-                const res = await csrfFetch('/api/products/')
+                const res = await csrfFetch('/api/products')
                 if(res.ok) {
                     const data = await res.json()
                     setProducts(data)
                 } else {
-                    console.log('There was an error fetching products')
+                    console.log('There was an error fetching products', csrfFetch)
                 }
             } catch (error) {
                 console.error("Error fetching products", error)
